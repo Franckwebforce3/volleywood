28dc64fac65ba5f3386d9ef03699c693f004f77b
Jerome-Neri <jerome.neri.pro@gmail.com>
2020-01-17 15:36:16 +0100

    ajout photos sur la table article modif table new crud art

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -34,7 +34,7 @@ class Article
     private $photo;
 
     /**
-     * @ORM\Column(type="string", length=160)
+     * @ORM\Column(type="text")
      */
     private $categorie;
 
@@ -47,6 +47,21 @@ class Article
      * @ORM\ManyToMany(targetEntity="App\Entity\Commentaire", mappedBy="articles")
      */
     private $commentaires;
+
+    /**
+     * @ORM\Column(type="string", length=160, nullable=true)
+     */
+    private $photo2;
+
+    /**
+     * @ORM\Column(type="string", length=160, nullable=true)
+     */
+    private $photo3;
+
+    /**
+     * @ORM\Column(type="string", length=160, nullable=true)
+     */
+    private $photo4;
 
     public function __construct()
     {
@@ -142,6 +157,42 @@ class Article
             $this->commentaires->removeElement($commentaire);
             $commentaire->removeArticle($this);
         }
+
+        return $this;
+    }
+
+    public function getPhoto2(): ?string
+    {
+        return $this->photo2;
+    }
+
+    public function setPhoto2(?string $photo2): self
+    {
+        $this->photo2 = $photo2;
+
+        return $this;
+    }
+
+    public function getPhoto3(): ?string
+    {
+        return $this->photo3;
+    }
+
+    public function setPhoto3(?string $photo3): self
+    {
+        $this->photo3 = $photo3;
+
+        return $this;
+    }
+
+    public function getPhoto4(): ?string
+    {
+        return $this->photo4;
+    }
+
+    public function setPhoto4(?string $photo4): self
+    {
+        $this->photo4 = $photo4;
 
         return $this;
     }
