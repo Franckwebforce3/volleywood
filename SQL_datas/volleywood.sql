95740ccf6f8e198884c52e17b6fb0278073fac0d
Jerome-Neri <jerome.neri.pro@gmail.com>
2020-01-22 17:15:49 +0100

    ajout de toute la base

--- SQL_datas/volleywood.sql
+++ SQL_datas/volleywood.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 22 jan. 2020 à 16:02
+-- Généré le :  mer. 22 jan. 2020 à 16:15
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.3.5
 
@@ -56,8 +56,211 @@ INSERT INTO `article` (`id`, `titre`, `description`, `photo`, `categorie`, `date
 (8, 'Tournoi 6x6 en salle', 'En Hivers et durant les vacances les clubs organisent des tournois en salle 4x4 mixte ou 6x6. Nous organisons les co-voiturages, les compositions des équipes si il manque des joueurs dans les équipes...', 'tournoi6x660-5e28649630ae8.jpeg', 'pour passer des journées à s\'éclater et repartir les bras chargés de cadeaux ou de bons souvenirs.', '2020-01-22 15:04:56', 'tournoi6x661-5e2864963337a.jpeg', 'tournoi6x663-5e286496aef7a.jpeg', 'tournoi6x662-5e286497363d6.jpeg', 'tournoi6x662-5e286497af0de.jpeg'),
 (9, 'Shooting pour la collection Volleywood', 'Vous souhaitez porter nos couleurs ?', 'shooting60-5e2865104b31a.jpeg', 'Nous avons les casquettes, tee-shirts et débardeurs, Homme, Femme... grace à la boutique officielle.', '2020-01-22 15:06:58', 'shooting61-5e2865104e0a3.jpeg', 'shooting63-5e286510cbdaf.jpeg', 'shooting62-5e286511542e4.jpeg', 'shooting62-5e286511d0ded.jpeg'),
 (10, 'La Tournée Volleywood', 'à partir du printemps de nombreux événements volley s\'offre à nous et nous avons nos tournois favoris à ne pas rater.\r\n\r\nAnnecy, Briançon, Digne Greoux-les-bains, Toulon, St Clair Lavandou, Fos Cavaou, Le Pradet, Le Luc ...', 'tournee60-5e286582c9d0c.jpeg', 'N\'hésitez pas à nous contacter si vous souhaitez faire partie de l\'aventure et découvrir les meilleurs tournois en France.', '2020-01-22 15:08:52', 'tournee61-5e286582cbc66.jpeg', 'tournee63-5e28658355679.jpeg', 'tournee62-5e286583d1bd0.jpeg', 'tournee62-5e286584581ff.jpeg'),
-(11, 'Tournoi mixte 3x3', 'Nous avons pu organiser l\'événement parfait ce 30 et 31 Mars 2019. Avec la célèbre Coach FIVB et joueuse pro internationale Olga Troshina qui vient spécialement pour l\'occasion sur Marseille.', 'le3x360-5e2865dcf17dd.jpeg', 'Lors du Tournoi Special 3x3 et le stage de perfectionnement.', '2020-01-22 15:10:23', 'le3x361-5e2865dcf395a.jpeg', 'le3x363-5e2865dd7cab3.jpeg', 'le3x362-5e2865de07c4c.jpeg', 'le3x362-5e2865de80986.jpeg'),
-(12, 'yvdrhtgbvkjfhvdydtufgusrwfgdrd', 'grdsvurthdwgrvtxickjjjjjjjjjjjjjjjjjjjjjjjjjjjjfcserhdgckrytxd', 'Ken-le-survivant-5e286dad3e630.jpeg', 'bvurtdvgerxdjhnoluytrbhbcçovèukl,tdrhgdfscwx', '2020-01-22 15:43:43', 'decouvrez-pourquoi-l-alcool-est-interdit-dans-l-espace_0c19eaa8443c1d49ce45c31ecd61b67c92c431ec-5e286dad41afe.jpeg', '300x300-5e286dadbf77a.jpeg', 'My-hero-academia-season-4-mha-1024x569-5e286dae47cdb.jpeg', 'SAUCISSONCORSEFLEURI-5e286daec1fa8.jpeg');
+(11, 'Tournoi mixte 3x3', 'Nous avons pu organiser l\'événement parfait ce 30 et 31 Mars 2019. Avec la célèbre Coach FIVB et joueuse pro internationale Olga Troshina qui vient spécialement pour l\'occasion sur Marseille.', 'le3x360-5e2865dcf17dd.jpeg', 'Lors du Tournoi Special 3x3 et le stage de perfectionnement.', '2020-01-22 15:10:23', 'le3x361-5e2865dcf395a.jpeg', 'le3x363-5e2865dd7cab3.jpeg', 'le3x362-5e2865de07c4c.jpeg', 'le3x362-5e2865de80986.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+DROP TABLE IF EXISTS `commande`;
+CREATE TABLE IF NOT EXISTS `commande` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `etat` int(11) NOT NULL,
+  `date_livraison` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_6EEAA67DA76ED395` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande_produit`
+--
+
+DROP TABLE IF EXISTS `commande_produit`;
+CREATE TABLE IF NOT EXISTS `commande_produit` (
+  `commande_id` int(11) NOT NULL,
+  `produit_id` int(11) NOT NULL,
+  PRIMARY KEY (`commande_id`,`produit_id`),
+  KEY `IDX_DF1E9E8782EA2E54` (`commande_id`),
+  KEY `IDX_DF1E9E87F347EFB` (`produit_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaire`
+--
+
+DROP TABLE IF EXISTS `commentaire`;
+CREATE TABLE IF NOT EXISTS `commentaire` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `users_id` int(11) NOT NULL,
+  `poce_bleu` int(11) DEFAULT NULL,
+  `message` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_parent` int(11) DEFAULT NULL,
+  `date_publication` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_67F068BC67B3B43D` (`users_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaire_article`
+--
+
+DROP TABLE IF EXISTS `commentaire_article`;
+CREATE TABLE IF NOT EXISTS `commentaire_article` (
+  `commentaire_id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  PRIMARY KEY (`commentaire_id`,`article_id`),
+  KEY `IDX_71F29C35BA9CD190` (`commentaire_id`),
+  KEY `IDX_71F29C357294869C` (`article_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `magasin`
+--
+
+DROP TABLE IF EXISTS `magasin`;
+CREATE TABLE IF NOT EXISTS `magasin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produits_id` int(11) DEFAULT NULL,
+  `quantite` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_54AF5F27CD11A2CF` (`produits_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `migration_versions`
+--
+
+DROP TABLE IF EXISTS `migration_versions`;
+CREATE TABLE IF NOT EXISTS `migration_versions` (
+  `version` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `executed_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `migration_versions`
+--
+
+INSERT INTO `migration_versions` (`version`, `executed_at`) VALUES
+('20200122125558', '2020-01-22 12:56:13');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `photo`
+--
+
+DROP TABLE IF EXISTS `photo`;
+CREATE TABLE IF NOT EXISTS `photo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produit_id` int(11) DEFAULT NULL,
+  `nom` varchar(160) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_14B78418F347EFB` (`produit_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+DROP TABLE IF EXISTS `produit`;
+CREATE TABLE IF NOT EXISTS `produit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(160) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `categorie` varchar(160) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `taille` varchar(160) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `couleur` varchar(160) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `prix_vente` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` json NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pseudo` varchar(160) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `avatar` varchar(160) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ville` varchar(160) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cp` varchar(5) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `adresse` varchar(160) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cle_activation` varchar(160) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `pseudo`, `avatar`, `ville`, `cp`, `adresse`, `cle_activation`) VALUES
+(1, 'jerome@mail.me', '[\"ROLE_ADMIN\"]', '$argon2id$v=19$m=65536,t=4,p=1$ZTFBdFZwb2Y1anFZbUlrQw$L7WtVd46lv2rnjRqnJzPYW4oioTDrUwxMpg2/SZ/XZ4', 'jerome', 'decouvrez-pourquoi-l-alcool-est-interdit-dans-l-espace_0c19eaa8443c1d49ce45c31ecd61b67c92c431ec-5e2846d5141a9.jpeg', 'adjfhldskgrvb', '12345', 'dhgdycjh(dytrujhrytfgdvjgj', 'd7f2202e1f9a65a877fa143f21c1188e');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD CONSTRAINT `FK_6EEAA67DA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `commande_produit`
+--
+ALTER TABLE `commande_produit`
+  ADD CONSTRAINT `FK_DF1E9E8782EA2E54` FOREIGN KEY (`commande_id`) REFERENCES `commande` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_DF1E9E87F347EFB` FOREIGN KEY (`produit_id`) REFERENCES `produit` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD CONSTRAINT `FK_67F068BC67B3B43D` FOREIGN KEY (`users_id`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `commentaire_article`
+--
+ALTER TABLE `commentaire_article`
+  ADD CONSTRAINT `FK_71F29C357294869C` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_71F29C35BA9CD190` FOREIGN KEY (`commentaire_id`) REFERENCES `commentaire` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `magasin`
+--
+ALTER TABLE `magasin`
+  ADD CONSTRAINT `FK_54AF5F27CD11A2CF` FOREIGN KEY (`produits_id`) REFERENCES `produit` (`id`);
+
+--
+-- Contraintes pour la table `photo`
+--
+ALTER TABLE `photo`
+  ADD CONSTRAINT `FK_14B78418F347EFB` FOREIGN KEY (`produit_id`) REFERENCES `produit` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
