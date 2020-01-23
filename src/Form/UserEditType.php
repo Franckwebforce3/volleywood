84ba7c5b89ffb39359e3d2c4dbde825cac7ddb91
Jerome-Neri <jerome.neri.pro@gmail.com>
2020-01-23 14:44:44 +0100

    modi jé

--- src/Form/UserEditType.php
+++ src/Form/UserEditType.php
@@ -14,14 +14,31 @@ class UserEditType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles', ChoiceType::class, [
-                'choices' => [
-                    'Role-membre'         => '["ROLE_MEMBRE"]',
-                    'Role-administrateur' => '["ROLE_ADMIN"]',
-                ],
-            ])
+            ->add('roles', ChoiceType::class, 
+                [
+                    'choices'  => ['User' => 'ROLE_USER',
+                                    'Membre' => 'ROLE_MEMBRE',                       
+                                    'Admin' => 'ROLE_ADMIN',                                        
+                    ],               
+                    'multiple'=>true,               
+                    'expanded'=>true,               
+                    'data'=> ['User', 'Membre', 'Admin'],//clé(index) du tableau choice               
+                'choice_attr' => function($choice, $key, $value) {                   
+                    // ajout d'un sélecteur de class html(.tous_les_contrats ou .contrats_choisis) aux champs                   
+                    $class = $key == 'Tous' ? 'tous_les_contrats': 'contrats_choisis';                   
+                    return ['class' => $class];               
+                },                
+                ] 
+            )
+            // ->add('roles', ChoiceType::class, [
+            //     'choices' => [
+            //         'Role-user'         => '["ROLE_USER"]',
+            //         'Role-membre'         => '["ROLE_MEMBRE"]',
+            //         'Role-administrateur' => '["ROLE_ADMIN"]',
+            //     ],
+            // ])
             ->add('pseudo')
-            ->add('avatar')
+            // ->add('avatar')
             ->add('ville')
             ->add('cp')
             ->add('adresse')
