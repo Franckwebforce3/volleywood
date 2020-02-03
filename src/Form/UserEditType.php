c184d651f9a8130b3ae52170669be1c05ca8bf73
Jerome-Neri <jerome.neri.pro@gmail.com>
2020-02-03 17:14:09 +0100

    ça y est les checkboxs fonctionne sur edit

--- src/Form/UserEditType.php
+++ src/Form/UserEditType.php
@@ -21,22 +21,9 @@ class UserEditType extends AbstractType
                                     'Admin' => 'ROLE_ADMIN',                                        
                     ],               
                     'multiple'=>true,               
-                    'expanded'=>true,               
-                    'data'=> ['User', 'Membre', 'Admin'],//clé(index) du tableau choice               
-                'choice_attr' => function($choice, $key, $value) {                   
-                    // ajout d'un sélecteur de class html(.tous_les_contrats ou .contrats_choisis) aux champs                   
-                    $class = $key == 'Tous' ? 'tous_les_contrats': 'contrats_choisis';                   
-                    return ['class' => $class];               
-                },                
+                    'expanded'=>true,                               
                 ] 
             )
-            // ->add('roles', ChoiceType::class, [
-            //     'choices' => [
-            //         'Role-user'         => '["ROLE_USER"]',
-            //         'Role-membre'         => '["ROLE_MEMBRE"]',
-            //         'Role-administrateur' => '["ROLE_ADMIN"]',
-            //     ],
-            // ])
             ->add('pseudo')
             // ->add('avatar')
             ->add('ville')
